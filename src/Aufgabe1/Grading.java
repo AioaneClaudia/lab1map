7d1e8c295761d35973d76f567087baf23677c2e0
Claudia <claudiavalentina2005@gmail.com>
2025-10-18 20:03:40 +0300

    Aufgabe1(tile 2,3,4)

--- src/Aufgabe1/Grading.java
+++ src/Aufgabe1/Grading.java
@@ -18,4 +18,47 @@ public class Grading {
         }
         return failing_grades;
     }
+
+    public static double Average(int[] grades) {
+        if (grades.length == 0) {
+            return 0.0;
+        }
+        double sum = 0;
+        for(int grade : grades) {
+            sum+=grade;
+        }
+        return (double)sum/grades.length;
+    }
+
+    public static int[] RoundedGrades(int[] grades) {
+        int[] rounded_grades = new int[grades.length];
+        int number=0;
+        int next_multiple_of_5=0;
+        for(int grade : grades) {
+            if(grade%10<5){
+                next_multiple_of_5=grade/10*10+5;
+            }
+            else
+                next_multiple_of_5=grade/10*10+10;
+
+            if(next_multiple_of_5-grade<3 && grade >=38) {
+                rounded_grades[number] = next_multiple_of_5;
+                }
+            else
+                rounded_grades[number] = grade;
+            number++;
+        }
+        return rounded_grades;
+    }
+
+    public static int highest_rounded_grade(int[] grades) {
+         int[] rounded_grades = RoundedGrades(grades);
+         int max=rounded_grades[0];
+         for(int grade : rounded_grades) {
+             if(grade>max) {
+                 max=grade;
+             }
+         }
+         return max;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
     int[] grades={29, 37, 38, 41, 84, 67};
+
     int[] failing_grades=Grading.Failing(grades);
     System.out.println("Failing Grades:");
     for (int i=0;i<failing_grades.length;i++){
         System.out.print(failing_grades[i]+" ");
     }
+    System.out.println();
+    System.out.println("Average:"+Grading.Average(grades));
+
+    int[] rounded_grades=Grading.RoundedGrades(grades);
+    System.out.println("Rounded Grades:");
+    for (int i=0;i<rounded_grades.length;i++){
+        System.out.print(rounded_grades[i]+" ");
+    }
+
+    System.out.println();
+    System.out.println("Highest Rounded Grade:"+Grading.highest_rounded_grade(grades));
+
     }
 }
 
